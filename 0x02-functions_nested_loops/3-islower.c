314b0f9e60de0e530e2a0676ebcb757457590942
TK <tortoremmanuel60@gmail.com>
2023-12-20 10:26:57 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_islower - Checks lowercases
+ *@c: The letter to be checked
+ *Return: 1 for lowercase and 0 for anything else
+ */
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
